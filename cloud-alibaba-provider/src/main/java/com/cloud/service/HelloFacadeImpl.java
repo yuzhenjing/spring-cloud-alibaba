c0bbf3b7377d9b0d8ee848a786c2aa680a97edf2
yuzhenjing <yuzj@nala.com.cn>
2019-10-17 15:53:31 +0800

    add dubbo dependencies

--- cloud-alibaba-provider/src/main/java/com/cloud/service/HelloFacadeImpl.java
+++ cloud-alibaba-provider/src/main/java/com/cloud/service/HelloFacadeImpl.java
@@ -1,10 +1,12 @@
 package com.cloud.service;
 
 import com.cloud.HelloFacade;
+import org.apache.dubbo.config.annotation.Service;
 
+@Service(version = "1.0.0", group = "TEST-DEMO")
 public class HelloFacadeImpl implements HelloFacade {
     @Override
     public String sayHello(String name) {
-        return null;
+        return "你好 " + name;
     }
 }
