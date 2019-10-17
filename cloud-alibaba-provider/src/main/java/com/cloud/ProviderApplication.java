0d1f3f26227a9ba7f066ab37ca4e296f742946b7
yuzhenjing <yuzj@nala.com.cn>
2019-10-17 18:09:20 +0800

    add log

--- cloud-alibaba-provider/src/main/java/com/cloud/ProviderApplication.java
+++ cloud-alibaba-provider/src/main/java/com/cloud/ProviderApplication.java
@@ -2,11 +2,13 @@ package com.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author yzj
  * 服务端启动类
  */
+@EnableAsync
 @SpringCloudApplication
 public class ProviderApplication {
 
--- cloud-alibaba-provider/src/main/java/com/cloud/service/HelloFacadeImpl.java
+++ cloud-alibaba-provider/src/main/java/com/cloud/service/HelloFacadeImpl.java
@@ -1,12 +1,21 @@
 package com.cloud.service;
 
 import com.cloud.HelloFacade;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Service;
 
+@Slf4j
 @Service(version = "1.0.0", group = "TEST-DEMO")
 public class HelloFacadeImpl implements HelloFacade {
+
+
     @Override
     public String sayHello(String name) {
+        log.debug("dubbo 请求过来了");
         return "你好 " + name;
+    }
+
+    private void test() {
+        log.debug("打印日志");
     }
 }
