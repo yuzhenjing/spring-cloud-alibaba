1b07e004069aef7b1caeca12baabd6411e740589
yuzhenjing <yuzj@nala.com.cn>
2019-10-17 16:05:10 +0800

    add bootstrap class

--- cloud-alibaba-provider/src/main/java/com/cloud/ProviderApplication.java
+++ cloud-alibaba-provider/src/main/java/com/cloud/ProviderApplication.java
@@ -1,4 +1,18 @@
 package com.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+/**
+ * @author yzj
+ * 服务端启动类
+ */
+@SpringCloudApplication
 public class ProviderApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+
 }
